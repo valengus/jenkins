e75ae4408c177990398f60c4fe408fc30642eab4
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-01 13:03:12 +0300

    test

---
+++ jenkins/jobs/MyStandardDeclarativePipelineJob.groovy
@@ -0,0 +1,28 @@
+def createMyStandardDeclarativePipeline(Map templateParams) {   
+
+  pipeline {
+    agent any
+    parameters {
+      string(name: 'myInput', description: 'Some pipeline parameters')
+    }
+    stages {
+      stage('Stage one') {
+        steps {
+          script {
+            echo "Parameter from template creation: " + templateParams.someParam
+          }
+        }
+      }
+      stage('Stage two') {
+        steps {
+          script {
+            echo "Job input parameter: " + params.myInput
+          }
+        }
+      }
+    }
+  }
+
+}
+
+template.createMyStandardDeclarativePipeline(someParam: 'myParam')
