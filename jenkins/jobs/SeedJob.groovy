575890f2cc4b7ef0f610b5ef1cb36e90a55afd17
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-01 14:02:35 +0300

    commit

--- jenkins/jobs/SeedJob.groovy
+++ jenkins/jobs/SeedJob.groovy
@@ -1,17 +1,5 @@
-#!/usr/bin/env groovy
-import jenkins.model.*
 @Library('globalPipelineLibraries')
 
-def github_projects = [
-  "docker"
-]
-
-github_projects.each {
-  service = it
-  println("${service}")
-
-  pipelineJob("${service}") {
-    template.createDeclarativePipeline(someParam: 'myParam')
- }
-
+pipelineJob("docker") {
+  template.createDeclarativePipeline(someParam: 'myParam')
 }
