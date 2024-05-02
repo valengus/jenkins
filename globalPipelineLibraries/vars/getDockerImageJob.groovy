bc30b790dcebbbc257081e4b1f5d543866f23d76
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-02 19:12:56 +0300

    commit

--- globalPipelineLibraries/vars/getDockerImageJob.groovy
+++ globalPipelineLibraries/vars/getDockerImageJob.groovy
@@ -26,7 +26,16 @@ def call(Map pipelineParams) {
               echo "${item}"
             }
 
-            buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git')
+            pipelineJob('github/docker/docker') {
+              definition {
+                cps {
+                  script('''@Library('globalPipelineLibraries') _
+                  buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git')
+                  '''.stripIndent())
+                  sandbox()     
+                }
+              }
+            }
 
           }
         }
