80709e56d573754e7f175856db49203f27442e33
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-02 11:59:12 +0300

    commit

--- globalPipelineLibraries/vars/buildDockerImage.groovy
+++ globalPipelineLibraries/vars/buildDockerImage.groovy
@@ -84,7 +84,7 @@ def call(Map pipelineParams) {
 
       stage('7-Push') {
         steps {
-          sh "docker push ${params.DOCKER_IMAGE_PATH}/${params.DOCKER_IMAGE} --all-tags"
+          sh "docker push ${params.DOCKER_IMAGE_PATH}/${params.DOCKER_IMAGE}"
         }
       }
 
