38ce2702c4a9694163f482fc6ae24c3f9a411667
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-02 14:06:41 +0300

    commit

--- jobs/dockerSeedJob.groovy
+++ jobs/dockerSeedJob.groovy
@@ -8,25 +8,25 @@ folder('github/docker') {
     description('https://github.com/valengus/docker.git')
 }
 
-job('TestJob') {
-    scm {
-        git('https://github.com/valengus/docker.git')
-    }
-    triggers {
-        scm('H/15 * * * *')
-    }
-    steps {
+// job('TestJob') {
+//     scm {
+//         git('https://github.com/valengus/docker.git')
+//     }
+//     triggers {
+//         scm('H/15 * * * *')
+//     }
+//     steps {
 
-      pipelineJob('github/docker/docker') {
-        definition {
-          cps {
-          script('''@Library('globalPipelineLibraries') _
-          buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git')
-          '''.stripIndent())
-          sandbox()     
-          }
-        }
-      }
+//       pipelineJob('github/docker/docker') {
+//         definition {
+//           cps {
+//           script('''@Library('globalPipelineLibraries') _
+//           buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git')
+//           '''.stripIndent())
+//           sandbox()     
+//           }
+//         }
+//       }
 
-    }
-}
+//     }
+// }
