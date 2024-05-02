203c7948f2f48f70410ee8124a04d844a764c0cc
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-02 23:13:58 +0300

    commit

--- jobs/seedJob.groovy
+++ jobs/seedJob.groovy
@@ -27,6 +27,17 @@ def list = [
 list.each {
   println it
 
+  pipelineJob('github/docker/$it') {
+    definition {
+      cps {
+        script('''@Library('globalPipelineLibraries') _
+        buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: "$it")
+        '''.stripIndent())
+        sandbox()     
+      }
+    }
+  }
+
 }
 
 
