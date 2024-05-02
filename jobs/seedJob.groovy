95982c718c707b18894f94af26c382ed94cefafb
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-02 11:10:24 +0300

    commit

--- jobs/seedJob.groovy
+++ jobs/seedJob.groovy
@@ -3,6 +3,10 @@ folder('github') {
     description('Projects stored on github')
 }
 
+folder('github/docker') {
+    displayName('docker')
+    description('https://github.com/valengus/docker.git')
+}
 
 pipelineJob('github/docker') {
   definition {
