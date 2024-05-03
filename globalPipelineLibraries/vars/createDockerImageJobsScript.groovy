7a03145489f432d71d5c076d015e680934ba8c60
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-03 10:00:56 +0300

    commit

--- globalPipelineLibraries/vars/createDockerImageJobsScript.groovy
+++ globalPipelineLibraries/vars/createDockerImageJobsScript.groovy
@@ -1,25 +1,20 @@
 def call(Map pipelineParams) {
 
   node {
-
     def dockerProjects = sh(script: "find . -maxdepth 1 -type d -name '*:*' | cut -c 3-", returnStdout: true).split('\n')
-    
-    dockerProjects.each { item ->
-      echo "${item}"
-    
-      jenkinsDockerBuildJobName = item.replace(":", "_")
+  }
 
-      pipelineJob("${jenkinsDockerBuildJobName}") {
-        definition {
-          cps {
-            script("@Library('globalPipelineLibraries') _ ;  buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: \"${item}\")".stripIndent())
-            sandbox()     
-          }
+  dockerProjects.each { item ->
+    echo "${item}"
+    jenkinsDockerBuildJobName = item.replace(":", "_")
+    pipelineJob("${jenkinsDockerBuildJobName}") {
+      definition {
+        cps {
+          script("@Library('globalPipelineLibraries') _ ;  buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: \"${item}\")".stripIndent())
+          sandbox()     
         }
       }
-
     }
-
   }
 
 }
