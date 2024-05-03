3fd6007903c3f5f46ec965fe8a6c08daa72a399b
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-03 17:30:49 +0300

    commit

--- jobs/seedJob.groovy
+++ jobs/seedJob.groovy
@@ -10,23 +10,21 @@ folder('github/docker') {
 
 
 
-def map = [ 
+def dockerBuildJobs = [ 
   "oraclelinux9": null, 
   "ansible2.15": "oraclelinux9",
 ]
 
-map.eachWithIndex{
+dockerBuildJobs.eachWithIndex{
   entry, i -> 
   println "$i $entry.key: $entry.value"
 
   pipelineJob("github/docker/$entry.key") {
 
-    if (entry.value == null || entry.value.isEmpty()) {
-    } else {
-      triggers {
-          upstream("$entry.value", 'SUCCESS')
-      }
-    }
+    if (entry.value != null) {
+      triggers { upstream("$entry.value", 'SUCCESS') }
+    } 
+
     definition {
       cps {
         script("@Library('globalPipelineLibraries') _ ; buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: \"$entry.key\")".stripIndent())
@@ -34,6 +32,5 @@ map.eachWithIndex{
       }
     }
   }
-
 
 }
