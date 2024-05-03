82e94fb669bed21649a725f7528e757a19eadf8a
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-03 14:41:46 +0300

    commit

--- globalPipelineLibraries/vars/buildDockerImageJob.groovy
+++ globalPipelineLibraries/vars/buildDockerImageJob.groovy
@@ -13,11 +13,17 @@ def call(Map pipelineParams) {
       BUILDTIME = sh(script: "echo `date +%F_%H%M%S`", returnStdout: true).trim()
     }
 
-    if (pipelineParams.docker_image_from != null && pipelineParams.docker_image_from != '') {
-      triggers {
-        upstream(upstreamProjects: "/github/docker/${pipelineParams.docker_image_from}", threshold: hudson.model.Result.SUCCESS)
-      }
+
+    triggers {
+      upstream(upstreamProjects: "/github/docker/${pipelineParams.docker_image_from}", threshold: hudson.model.Result.SUCCESS)
+      when { expression { pipelineParams.docker_image_from != null } }
     }
+
+    // if (pipelineParams.docker_image_from != null && pipelineParams.docker_image_from != '') {
+    //   triggers {
+    //     upstream(upstreamProjects: "/github/docker/${pipelineParams.docker_image_from}", threshold: hudson.model.Result.SUCCESS)
+    //   }
+    // }
 
 
     stages {
