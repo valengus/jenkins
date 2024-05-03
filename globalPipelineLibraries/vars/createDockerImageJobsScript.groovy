263024b97e10510e64396293d2dc9cd2f1938e5b
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-03 10:19:15 +0300

    commit

--- globalPipelineLibraries/vars/createDockerImageJobsScript.groovy
+++ globalPipelineLibraries/vars/createDockerImageJobsScript.groovy
@@ -16,3 +16,4 @@ def call(Map pipelineParams) {
   }
 
 }
+
--- jobs/seedJob.groovy
+++ jobs/seedJob.groovy
@@ -8,50 +8,32 @@ folder('github/docker') {
     description('https://github.com/valengus/docker.git')
 }
 
+def list = [ 
+  "ansible:2.15.6",
+  "oraclelinux:9",
+  "ansible:2.7.8",
+  "oraclelinux:8",
+  "python:3.9",
+  "ansible:molecule",
+  "terraform:0.13.7",
+  "oraclelinux-systemd:8",
+  "s3fs-fuse:1.93-1",
+  "jenkins:2.346.2",
+  "centos:7",
+  "php:8.1"
+]
 
-pipelineJob("dockerBuildSeedJob") {
-  definition {
-    cps {
-      script("@Library('globalPipelineLibraries') _ ;  checkoutJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git')".stripIndent())
-      sandbox()     
+list.each { item ->
+
+  jenkinsDockerBuildJobName = item.replace(":", "_")
+
+  pipelineJob("github/docker/${jenkinsDockerBuildJobName}") {
+    definition {
+      cps {
+        script("@Library('globalPipelineLibraries') _ ;  buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: \"${item}\")".stripIndent())
+        sandbox()     
+      }
     }
   }
+
 }
-
-
-
-
-
-
-
-
-
-// def list = [ 
-//   "ansible:2.15.6",
-//   "oraclelinux:9",
-//   "ansible:2.7.8",
-//   "oraclelinux:8",
-//   "python:3.9",
-//   "ansible:molecule",
-//   "terraform:0.13.7",
-//   "oraclelinux-systemd:8",
-//   "s3fs-fuse:1.93-1",
-//   "jenkins:2.346.2",
-//   "centos:7",
-//   "php:8.1"
-// ]
-
-// list.each { item ->
-
-//   jenkinsDockerBuildJobName = item.replace(":", "_")
-
-//   pipelineJob("github/docker/${jenkinsDockerBuildJobName}") {
-//     definition {
-//       cps {
-//         script("@Library('globalPipelineLibraries') _ ;  buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: \"${item}\")".stripIndent())
-//         sandbox()     
-//       }
-//     }
-//   }
-
-// }
