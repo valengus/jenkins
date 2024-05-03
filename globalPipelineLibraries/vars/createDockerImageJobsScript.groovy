8d70606d701ecd3127daa6a7a005a9c22b5bdd3d
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-03 10:04:03 +0300

    commit

--- globalPipelineLibraries/vars/createDockerImageJobsScript.groovy
+++ globalPipelineLibraries/vars/createDockerImageJobsScript.groovy
@@ -1,8 +1,6 @@
 def call(Map pipelineParams) {
 
-  node {
-    def dockerProjects = sh(script: "find . -maxdepth 1 -type d -name '*:*' | cut -c 3-", returnStdout: true).split('\n')
-  }
+  def dockerProjects = sh(script: "find . -maxdepth 1 -type d -name '*:*' | cut -c 3-", returnStdout: true).split('\n')
 
   dockerProjects.each { item ->
     echo "${item}"
