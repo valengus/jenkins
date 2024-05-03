8f129883e6f30b77e5a582d63b3bbbc12bc903f3
Gusachenko Valentin <vgusachenko@gmail.com>
2024-05-03 14:30:59 +0300

    commit

--- jobs/seedJob.groovy
+++ jobs/seedJob.groovy
@@ -19,7 +19,7 @@ map.eachWithIndex{
   entry, i -> 
   println "$i $entry.key: $entry.value"
 
-  pipelineJob("github/docker/${item}") {
+  pipelineJob("github/docker/$entry.key") {
     definition {
       cps {
         script("@Library('globalPipelineLibraries') _ ; buildDockerImageJob(branch: 'main', git_url: 'https://github.com/valengus/docker.git', docker_image: \"$entry.key\", docker_image_from: \"$entry.value\")".stripIndent())
